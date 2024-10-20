ec487a2b87e15a5151590423ea12957a9500ce64
MachadoLMX <lucas.mmds@hotmail.com>
2024-10-20 19:37:39 -0300

    Desafio 01: Saudações Personalizadas

---
+++ Desafio01.py
@@ -0,0 +1,8 @@
+# Imprime o título do desafio
+print('====== DESAFIO 01 ======')
+
+# Solicita ao usuário que insira seu nome e armazena na variável 'nome'
+nome = input('Qual é o seu nome? ')
+
+# Exibe uma mensagem de boas-vindas personalizada com o nome inserido pelo usuário
+print('Olá', nome,'!', 'Prazer em te conhecer!')
