fa050c0f59cb2d663beba2a3368b84661ef4f5c1
MachadoLMX <lucas.mmds@hotmail.com>
2024-10-20 19:38:32 -0300

    Desafio 03: Soma de Dois Números

---
+++ Desafio03.py
@@ -0,0 +1,14 @@
+# Imprime o título do desafio
+print('====== DESAFIO 03 ======')
+
+# Solicita ao usuário que insira o primeiro número, convertendo-o para inteiro
+v1 = (int( input('Digite um número: ')))
+
+# Solicita ao usuário que insira o segundo número, convertendo-o para inteiro
+v2 = (int( input('Digite um número: ')))
+
+# Realiza a soma dos dois números e armazena o resultado na variável 's'
+s = v1 +
+
+# Exibe o resultado da soma usando o metodo format para interpolação de variáveis
+print ('A soma de {} e {} é = {}'.format(v1, v2, s))
