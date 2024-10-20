bd87faddbb74cb2e26e03f79d2033bb170092969
MachadoLMX <lucas.mmds@hotmail.com>
2024-10-20 19:38:11 -0300

    Desafio 02: Captura de Data de Nascimento

---
+++ Desafio02.py
@@ -0,0 +1,14 @@
+# Imprime o título do desafio
+print('====== DESAFIO 02 ======')
+
+# Solicita ao usuário que insira o dia de nascimento e armazena na variável 'dia'
+dia = input('Digite o dia do seu nascimento: ')
+
+# Solicita ao usuário que insira o mês de nascimento e armazena na variável 'mes'
+mes = input('Digite o mês do seu nascimento: ')
+
+# Solicita ao usuário que insira o ano de nascimento e armazena na variável 'ano'
+ano = input('Digite o ano do seu nascimento: ')
+
+# Exibe a data de nascimento formatada e pergunta se está correta
+print ('você nasceu em: ', dia,'/',mes,'/',ano,'. Correto?')
