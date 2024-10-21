e054938ff204a7d0872908f8000cc855b1c0a3e0
MachadoLMX <lucas.mmds@hotmail.com>
2024-10-20 23:28:32 -0300

    Desafio 05: Sucessor e Antecessor de um Número Inteiro

---
+++ Desafio05.py
@@ -0,0 +1,10 @@
+# Imprime o título do desafio
+print('====== DESAFIO 05  ======')
+
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+
+num = int(input('Informe um número: '))
+sucessor = (num + 1)
+antecessor = (num - 1)
+
+print ('Seu número é = {}, seu sucessor é = {}, seu antecessor é = {}'.format(num, sucessor, antecessor))
